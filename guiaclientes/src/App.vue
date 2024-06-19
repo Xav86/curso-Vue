511ff03a2552e8b3d92c9e4617ef74f72b14e164
Xav86 <gustavo.g.2005@gmail.com>
2024-06-19 17:37:29 -0300

    V-for

--- guiaclientes/src/App.vue
+++ guiaclientes/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <input type="text" v-model="clienteUsuario.nome">
-    <input type="text" v-model="clienteUsuario.email">
     <!-- <input type="text" v-model="nomeUsuario"> -->
     <!-- <Cliente :nome="nomeUsuario" email="gustavo@gustavo.com" idade="19"/> -->
-    <Cliente :cliente="clienteUsuario" :showIdade="true"/>
-    <Cliente :cliente="clienteUsuario" :showIdade="false"/>
+
+    <div v-for="(cliente, index) in clientes" :key="cliente.id">
+      <h3>{{ index + 1 }}</h3>
+      <Cliente :cliente="cliente" :showIdade="true" />
+      <hr>
+      <h4>Edição: </h4>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
+
   </div>
 </template>
 
@@ -20,7 +26,45 @@ export default {
         nome: "Gustavo",
         email: "gustavo@gustavo.com",
         idade: 19
-      }
+      },
+      clientes: [
+        {
+          id: 1,
+          nome: "Cliente1",
+          email: "Cliente1@Cliente1.com",
+          idade: 124
+        },
+        {
+          id: 4,
+          nome: "Cliente2",
+          email: "Cliente2@Cliente2.com",
+          idade: 49
+        },
+        {
+          id: 34587,
+          nome: "Cliente3",
+          email: "Cliente3@Cliente3.com",
+          idade: 91
+        },
+        {
+          id: 3,
+          nome: "Cliente4",
+          email: "Cliente4@Cliente4.com",
+          idade: 32
+        },
+        {
+          id: 3,
+          nome: "Cliente4",
+          email: "Cliente4@Cliente4.com",
+          idade: 32
+        },
+        {
+          id: 56,
+          nome: "Cliente5.0",
+          email: "Cliente5@Cliente5.com",
+          idade: 32
+        }
+      ]
     }
   },
   components: {
